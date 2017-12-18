1fc912bcaf7bb9670a24c66dce6ba71fd0ac00ff
praveenjp <roadizpraveen@gmail.com>
2017-12-18 20:15:21 +0530

    file structure updated initial file and setup added

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+__version__ = '0.1'
+import os
+from flask import Flask
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+from app.user.controllers import *
