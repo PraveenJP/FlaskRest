4705c8291d36eca4373e7fada8075411036e2ca3
praveenjp <roadizpraveen@gmail.com>
2017-12-18 20:16:04 +0530

    Config file added

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 import os
 
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     DEBUG = False
     Testing = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'thisissecret'
+    SECRET_KEY = ';\x85\x0fd\x05\x96\xbf6`7nj\xb3\xc0U\xafF\xc5tL\xfdN\x93\xc8'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
