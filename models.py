53191b9e128b7fab335908eb8de2489b86e7a892
praveenjp <roadizpraveen@gmail.com>
2017-12-18 19:15:36 +0530

    Model file setup

---
+++ models.py
@@ -0,0 +1,45 @@
+from app import db, ma
+from sqlalchemy.dialects.postgresql import JSON
+
+
+class Result(db.Model):
+    __tablename__ = 'results'
+
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String())
+    result_all = db.Column(JSON)
+    result_no_stop_words = db.Column(JSON)
+
+    def __init__(self, url, result_all, result_no_stop_words):
+        self.url = url
+        self.result_all = result_all
+        self.result_no_stop_words = result_no_stop_words
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+class ResultSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('url', 'result_all', 'result_no_stop_words')
+
+result_schema = ResultSchema()
+results_schema = ResultSchema(many=True)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('username', 'email')
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
